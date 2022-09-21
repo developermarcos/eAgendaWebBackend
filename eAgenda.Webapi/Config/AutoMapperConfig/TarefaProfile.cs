4303ef3bbcebb2046c06e6701537133c15892fc3
Marcos Adriano Ribeiro Lima <developermarcos@outlook.com>
2022-09-21 16:58:12 -0300

    Mapeador view model tarefas implementado

---
+++ eAgenda.Webapi/Config/AutoMapperConfig/TarefaProfile.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using eAgenda.Dominio.Compartilhado;
+using eAgenda.Dominio.ModuloTarefa;
+using eAgenda.Webapi.ViewModels.Tarefas;
+
+namespace eAgenda.Webapi.CreateMap.AutoMapperCreateMap
+{
+    public class TarefaProfile : Profile
+    {
+        public TarefaProfile()
+        {
+            //definições de conversão
+
+            ConverterEntidadeParaViewModel();
+            ConverterViewModelParaEntidade();
+        }
+
+        private void ConverterViewModelParaEntidade()
+        {
+            CreateMap<InserirTarefaViewModel, Tarefa>()
+                .ForMember(destino => destino.Itens, opt => opt.Ignore())
+                .AfterMap((viewModel, tarefa) =>
+                {
+                    foreach (var itemVM in viewModel.Itens)
+                    {
+                        var item = new ItemTarefa();
+                        item.Titulo = itemVM.Titulo;
+                        tarefa.AdicionarItem(item);
+                    }
+                });
+            CreateMap<EditarTarefaViewModel, Tarefa>()
+                .ForMember(destino => destino.Itens, opt => opt.Ignore())
+                .AfterMap((viewModel, tarefa) =>
+                {
+                    foreach (var itemVM in viewModel.Itens)
+                    {
+                        if (itemVM.Concluido)
+                            tarefa.ConcluirItem(itemVM.Id);
+                        else
+                            tarefa.MarcarPendente(itemVM.Id);
+                    }
+
+                    foreach (var itemVM in viewModel.Itens)
+                    {
+                        if (itemVM.Status == StatusItemTarefa.Adicionado)
+                        {
+                            var item = new ItemTarefa(itemVM.Titulo);
+                            tarefa.AdicionarItem(item);
+                        }
+                        else
+                        {
+                            tarefa.RemoverItem(itemVM.Id);
+                        }
+                    }
+                });
+        }
+
+        private void ConverterEntidadeParaViewModel()
+        {
+            CreateMap<Tarefa, ListarTarefaViewModel>()
+                .ForMember(destino => destino.Prioridade, opt => opt.MapFrom(origem => origem.Prioridade.GetDescription()))
+                .ForMember(destino => destino.Situacao, opt => opt.MapFrom(origen => origen.PercentualConcluido == 100 ? "Concluído" : "Pendente"));
+
+
+            CreateMap<Tarefa, VisualizarTarefaViewModel>()
+                .ForMember(destino => destino.Prioridade, opt => opt.MapFrom(origem => origem.Prioridade.GetDescription()))
+                .ForMember(destino => destino.Situacao, opt => opt.MapFrom(origen => origen.PercentualConcluido == 100 ? "Concluído" : "Pendente"))
+                .ForMember(destino => destino.QuantidadeItens, opt => opt.MapFrom(origem => origem.Itens.Count));
+
+            CreateMap<ItemTarefa, VisualizarItemTarefaViewModel>()
+                .ForMember(destino => destino.Status, opt => opt.MapFrom(origen => origen.Concluido ? "Concluído" : "Pendente"));
+        }
+    }
+}
--- eAgenda.Webapi/Controllers/TarefasController.cs
+++ eAgenda.Webapi/Controllers/TarefasController.cs
@@ -1,12 +1,11 @@
 ﻿using AutoMapper;
 using eAgenda.Aplicacao.ModuloTarefa;
-using eAgenda.Dominio.Compartilhado;
 using eAgenda.Dominio.ModuloTarefa;
 using eAgenda.Infra.Configs;
 using eAgenda.Infra.Orm;
 using eAgenda.Infra.Orm.ModuloTarefa;
+using eAgenda.Webapi.CreateMap.AutoMapperCreateMap;
 using eAgenda.Webapi.ViewModels.Tarefas;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -29,59 +28,7 @@ namespace eAgenda.Webapi.Controllers
 
             var autoMappeConfig = new MapperConfiguration(config =>
             {
-                //definições de conversão
-
-                //Da entidade para o view model
-                config.CreateMap<Tarefa, ListarTarefaViewModel>()
-                    .ForMember(destino => destino.Prioridade, opt => opt.MapFrom(origem => origem.Prioridade.GetDescription()))
-                    .ForMember(destino => destino.Situacao, opt => opt.MapFrom(origen => origen.PercentualConcluido == 100 ? "Concluído" : "Pendente"));
-
-
-                config.CreateMap<Tarefa, VisualizarTarefaViewModel>()
-                    .ForMember(destino => destino.Prioridade, opt => opt.MapFrom(origem => origem.Prioridade.GetDescription()))
-                    .ForMember(destino => destino.Situacao, opt => opt.MapFrom(origen => origen.PercentualConcluido == 100 ? "Concluído" : "Pendente"))
-                    .ForMember(destino => destino.QuantidadeItens, opt => opt.MapFrom(origem => origem.Itens.Count));
-
-                config.CreateMap<ItemTarefa, VisualizarItemTarefaViewModel>()
-                    .ForMember(destino => destino.Status, opt => opt.MapFrom(origen => origen.Concluido ? "Concluído" : "Pendente"));
-
-                //Do view model para entidade
-                config.CreateMap<InserirTarefaViewModel, Tarefa>()
-                    .ForMember(destino => destino.Itens, opt => opt.Ignore())
-                    .AfterMap((viewModel, tarefa) =>
-                    {
-                        foreach (var itemVM in viewModel.Itens)
-                        {
-                            var item = new ItemTarefa();
-                            item.Titulo = itemVM.Titulo;
-                            tarefa.AdicionarItem(item);
-                        }
-                    });
-                config.CreateMap<EditarTarefaViewModel, Tarefa>()
-                    .ForMember(destino => destino.Itens, opt => opt.Ignore())
-                    .AfterMap((viewModel, tarefa) =>
-                    {
-                        foreach (var itemVM in viewModel.Itens)
-                        {
-                            if (itemVM.Concluido)
-                                tarefa.ConcluirItem(itemVM.Id);
-                            else
-                                tarefa.MarcarPendente(itemVM.Id);
-                        }
-
-                        foreach (var itemVM in viewModel.Itens)
-                        {
-                            if (itemVM.Status == StatusItemTarefa.Adicionado)
-                            {
-                                var item = new ItemTarefa(itemVM.Titulo);
-                                tarefa.AdicionarItem(item);
-                            }
-                            else
-                            {
-                                tarefa.RemoverItem(itemVM.Id);
-                            }
-                        }
-                    });
+                config.AddProfile<TarefaProfile>();
 
             });
             mapeadorTarefas = autoMappeConfig.CreateMapper();
