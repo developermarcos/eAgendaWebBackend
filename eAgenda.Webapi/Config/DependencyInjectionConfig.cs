a3f33ffb8b9329acb8162b1ec43438241c0d4946
Marcos Adriano Ribeiro Lima <developermarcos@outlook.com>
2022-09-29 14:50:38 -0300

    Mensagens identity & config Startup refatorado

---
+++ eAgenda.Webapi/Config/DependencyInjectionConfig.cs
@@ -0,0 +1,34 @@
+﻿using eAgenda.Aplicacao.ModuloAutenticacao;
+using eAgenda.Aplicacao.ModuloContato;
+using eAgenda.Aplicacao.ModuloTarefa;
+using eAgenda.Dominio;
+using eAgenda.Dominio.ModuloContato;
+using eAgenda.Dominio.ModuloTarefa;
+using eAgenda.Infra.Configs;
+using eAgenda.Infra.Orm;
+using eAgenda.Infra.Orm.ModuloContato;
+using eAgenda.Infra.Orm.ModuloTarefa;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eAgenda.Webapi.Config
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void ConfigurarInjecaoDependencia(this IServiceCollection services)
+        {
+            services.AddSingleton((x) => new ConfiguracaoAplicacaoeAgenda().ConnectionStrings);
+
+            services.AddScoped<eAgendaDbContext>();
+
+            services.AddScoped<IContextoPersistencia, eAgendaDbContext>();
+
+            services.AddScoped<IRepositorioTarefa, RepositorioTarefaOrm>();
+            services.AddTransient<ServicoTarefa>();
+
+            services.AddScoped<IRepositorioContato, RepositorioContatoOrm>();
+            services.AddTransient<ServicoContato>();
+
+            services.AddTransient<ServicoAutenticacao>();
+        }
+    }
+}
--- eAgenda.Webapi/Startup.cs
+++ eAgenda.Webapi/Startup.cs
@@ -1,29 +1,10 @@
-using eAgenda.Aplicacao.ModuloAutenticacao;
-using eAgenda.Aplicacao.ModuloContato;
-using eAgenda.Aplicacao.ModuloTarefa;
-using eAgenda.Dominio;
-using eAgenda.Dominio.ModuloAutenticacao;
-using eAgenda.Dominio.ModuloContato;
-using eAgenda.Dominio.ModuloTarefa;
-using eAgenda.Infra.Configs;
-using eAgenda.Infra.Orm;
-using eAgenda.Infra.Orm.ModuloContato;
-using eAgenda.Infra.Orm.ModuloTarefa;
-using eAgenda.Webapi.Config.AutoMapperConfig;
-using eAgenda.Webapi.CreateMap.AutoMapperCreateMap;
-using eAgenda.Webapi.Filters;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using eAgenda.Webapi.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Text;
 
 namespace eAgenda.Webapi
 {
@@ -39,58 +20,25 @@ namespace eAgenda.Webapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentity<Usuario, IdentityRole<Guid>>()
-                .AddEntityFrameworkStores<eAgendaDbContext>()
-                .AddDefaultTokenProviders();
-            services.AddTransient<UserManager<Usuario>>();
-            services.AddTransient<SignInManager<Usuario>>();
-
             services.Configure<ApiBehaviorOptions>(config =>
             {
                 config.SuppressModelStateInvalidFilter = true;
             });
 
-            services.AddSingleton((x) => new ConfiguracaoAplicacaoeAgenda().ConnectionStrings);
-            
-            services.AddScoped<eAgendaDbContext>();
+            services.AddAutoMapper(typeof(Startup));
 
-            services.AddScoped<IContextoPersistencia, eAgendaDbContext>();
+            services.ConfigurarInjecaoDependencia();
 
-            MapeadoresViewModel(services);
+            services.ConfigurarAutenticacao();
 
-            Dependencias(services);
+            services.ConfigurarFiltros();
 
-            services.AddControllers(config =>
-            {
-                config.Filters.Add(new ValidarViewModelActionFilter());
-            });
+            services.ConfigurarSwagger();
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "eAgenda.Webapi", Version = "v1" });
-            });
-
-            var key = Encoding.ASCII.GetBytes("segredoSuperSecretoDoeAgenda");
-
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidAudience = "http://localhost",
-                    ValidIssuer = "eAgenda"
-                };
-            });
+            services.ConfigurarJwt();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -113,27 +61,6 @@ namespace eAgenda.Webapi
                 endpoints.MapControllers();
             });
         }
-        #region métodos privados
-        private static void Dependencias(IServiceCollection services)
-        {
-            services.AddScoped<IRepositorioTarefa, RepositorioTarefaOrm>();
-            services.AddTransient<ServicoTarefa>();
 
-            services.AddScoped<IRepositorioContato, RepositorioContatoOrm>();
-            services.AddTransient<ServicoContato>();
-
-            services.AddTransient<ServicoAutenticacao>();
-        }
-
-        private static void MapeadoresViewModel(IServiceCollection services)
-        {
-            services.AddAutoMapper(config =>
-            {
-                config.AddProfile<TarefaProfile>();
-                config.AddProfile<UsuarioProfile>();
-                config.AddProfile<ContatoProfile>();
-            });
-        }
-        #endregion
     }
 }
